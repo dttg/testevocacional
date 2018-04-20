b41a2e5a6e4397de7b9e95a7bec99be49d87a6e9
Luis Puhl <luispuhl@gmail.com>
2018-04-20 19:41:32 -0300

    Adicioanando novas fontes e comoponentes de angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
 import { QuestaoModule } from './questao/questao.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule,
+
     QuestaoModule,
   ],
   providers: [],
+  declarations: [
+    AppComponent,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
