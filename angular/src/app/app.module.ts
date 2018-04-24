a4d9a0de17673187b61fa251bb904cca6db3d841
Luis Puhl <luispuhl@gmail.com>
2018-04-24 12:54:12 -0300

    Adicionado toolbar superior

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material';
 
 import { QuestaoModule } from './questao/questao.module';
 
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatGridListModule,
+    MatToolbarModule,
 
     QuestaoModule,
   ],
