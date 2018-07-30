2d22a93898a5dafae9fb6d901fbd9918c2705b71
Luis Puhl <luispuhl@gmail.com>
2018-07-30 16:39:20 -0300

    Adicionando botão de compartinhar no facebook

--- angular/src/app/questionario/carreiras/carreiras.component.ts
+++ angular/src/app/questionario/carreiras/carreiras.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RiasecService } from '../riasec/riasec.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-carreiras',
@@ -8,10 +9,18 @@ import { RiasecService } from '../riasec/riasec.service';
 })
 export class CarreirasComponent implements OnInit {
   resultados = [];
+  facebookURL: SafeResourceUrl;
 
   constructor(
-    private riasecService: RiasecService
-  ) { }
+    private riasecService: RiasecService,
+    private sanitizer: DomSanitizer
+  ) {
+    this.facebookURL = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2F' +
+      window.location.hostname +
+      '&layout=button&size=large&mobile_iframe=true&appId=1604653659597605&width=117&height=28'
+    );
+  }
 
   ngOnInit() {
     this.resultados = this.riasecService.getCarreiras();
